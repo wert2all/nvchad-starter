4891e0d10080c1102f01e5f26f1c7c89cb6af2ae
Serhii Shvets <wert2all@gmail.com>
2025-03-13 18:17:33 +0200

    feat: add noice

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,72 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    presets = {
+      bottom_search = true, -- use a classic bottom cmdline for search
+      command_palette = true, -- position the cmdline and popupmenu together
+      long_message_to_split = true, -- long messages will be sent to a split
+      inc_rename = false, -- enables an input dialog for inc-rename.nvim
+      lsp_doc_border = false, -- add a border to hover docs and signature help
+    },
+    views = {
+      -- Clean cmdline_popup + palette
+      cmdline_popup = {
+        position = {
+          row = 10,
+          col = "50%",
+        },
+        border = {
+          style = "none",
+          padding = { 2, 3 },
+        },
+        size = {
+          min_width = 60,
+          width = "auto",
+          height = "auto",
+        },
+        win_options = {
+          winhighlight = { NormalFloat = "NormalFloat", FloatBorder = "FloatBorder" },
+        },
+      },
+      cmdline_popupmenu = {
+        relative = "editor",
+        position = {
+          row = 13,
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = "auto",
+          max_height = 15,
+        },
+        border = {
+          style = "none",
+          padding = { 0, 3 },
+        },
+        win_options = {
+          winhighlight = { NormalFloat = "NormalFloat", FloatBorder = "NoiceCmdlinePopupBorder" },
+        },
+      },
+      hover = {
+        border = {
+          style = "single",
+        },
+      },
+      confirm = {
+        border = {
+          style = "single",
+        },
+      },
+      popup = {
+        border = {
+          style = "single",
+        },
+      },
+    },
+  },
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
+}
