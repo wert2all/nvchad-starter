692a3ff828e126181abf9d62a7f02ec452306826
wert2all <wert2all@gmail.com>
2025-03-15 12:47:58 +0200

    fix: update key maps

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -15,6 +15,25 @@ map("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 map("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 map("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+-- better indenting
+map("v", "<", "<gv")
+map("v", ">", ">gv")
+
+-- paste over currently selected text without yanking it
+map("v", "p", '"_dp')
+map("v", "P", '"_dP')
+
+-- Navigate buffers
+map("n", "]b", ":bnext<CR>")
+map("n", "[b", ":bprevious<CR>")
+
+-- Keep cursor centered when scrolling
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+
+map("n", "n", "nzzv")
+map("n", "N", "Nzzv")
+
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
   group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
