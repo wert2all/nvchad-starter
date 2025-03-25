e6a804bc320d326ae81b4b26f1fcd51b5d7e12af
Serhii Shvets <wert2all@gmail.com>
2025-03-25 13:40:52 +0200

    feat: add vtsls LLP

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -28,6 +28,35 @@ local servers = {
       new_config.cmd = cmd
     end,
   },
+  vtsls = {
+    settings = {
+      typescript = {
+        updateImportsOnFileMove = { enabled = "always" },
+        inlayHints = {
+          parameterNames = { enabled = "all" },
+          parameterTypes = { enabled = true },
+          variableTypes = { enabled = true },
+          propertyDeclarationTypes = { enabled = true },
+          functionLikeReturnTypes = { enabled = true },
+          enumMemberValues = { enabled = true },
+        },
+      },
+      javascript = {
+        updateImportsOnFileMove = { enabled = "always" },
+        inlayHints = {
+          parameterNames = { enabled = "literals" },
+          parameterTypes = { enabled = true },
+          variableTypes = { enabled = true },
+          propertyDeclarationTypes = { enabled = true },
+          functionLikeReturnTypes = { enabled = true },
+          enumMemberValues = { enabled = true },
+        },
+      },
+      vtsls = {
+        enableMoveToFileCodeAction = true,
+      },
+    },
+  },
 }
 
 vim.lsp.enable(servers)
