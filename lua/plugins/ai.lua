f8f61eda98efa0ebc0989bdface4bc20c15d6c52
Serhii Shvets <wert2all@gmail.com>
2025-05-01 02:47:31 +0300

    feat: update ai

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,28 +1,73 @@
 return {
-  "Exafunction/codeium.vim",
-  cmd = {
-    "Codeium",
-    "CodeiumEnable",
-    "CodeiumDisable",
-    "CodeiumToggle",
-    "CodeiumAuto",
-    "CodeiumManual",
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", branch = "master" },
+      {
+        "nvim-treesitter/nvim-treesitter",
+        opts = function(_, opts)
+          if opts.ensure_installed ~= "all" then
+            opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "markdown" })
+          end
+        end,
+      },
+    },
+    opts = {
+      strategies = {
+        chat = {
+          adapter = "openrouter",
+        },
+        inline = {
+          adapter = "openrouter",
+        },
+        cmd = {
+          adapter = "openrouter",
+        },
+      },
+      adapters = {
+        openrouter = function()
+          return require("codecompanion.adapters").extend("openai_compatible", {
+            env = {
+              url = "https://openrouter.ai/api",
+              api_key = os.getenv "OPENROUTER_API_KEY",
+              chat_url = "/v1/chat/completions",
+            },
+            schema = {
+              model = {
+                default = "openrouter/optimus-alpha",
+              },
+            },
+          })
+        end,
+      },
+    },
   },
-  event = "BufEnter",
-  config = function(_, opts)
-    vim.keymap.set("n", "<Leader>;", "<Cmd>CodeiumToggle<CR>", { desc = "Toggle Codeium active" })
-
-    vim.keymap.set("i", "<C-g>", function()
-      return vim.fn["codeium#Accept"]()
-    end, { expr = true, silent = true })
-    vim.keymap.set("i", "<C-;>", function()
-      return vim.fn["codeium#CycleCompletions"](1)
-    end, { expr = true, silent = true })
-    vim.keymap.set("i", "<C-,>", function()
-      return vim.fn["codeium#CycleCompletions"](-1)
-    end, { expr = true, silent = true })
-    vim.keymap.set("i", "<C-x>", function()
-      return vim.fn["codeium#Clear"]()
-    end, { expr = true, silent = true })
-  end,
+  -- {
+  --   "Exafunction/codeium.vim",
+  --   cmd = {
+  --     "Codeium",
+  --     "CodeiumEnable",
+  --     "CodeiumDisable",
+  --     "CodeiumToggle",
+  --     "CodeiumAuto",
+  --     "CodeiumManual",
+  --   },
+  --   event = "BufEnter",
+  --   config = function(_, opts)
+  --     vim.keymap.set("n", "<Leader>;", "<Cmd>CodeiumToggle<CR>", { desc = "Toggle Codeium active" })
+  --
+  --     vim.keymap.set("i", "<C-g>", function()
+  --       return vim.fn["codeium#Accept"]()
+  --     end, { expr = true, silent = true })
+  --     vim.keymap.set("i", "<C-;>", function()
+  --       return vim.fn["codeium#CycleCompletions"](1)
+  --     end, { expr = true, silent = true })
+  --     vim.keymap.set("i", "<C-,>", function()
+  --       return vim.fn["codeium#CycleCompletions"](-1)
+  --     end, { expr = true, silent = true })
+  --     vim.keymap.set("i", "<C-x>", function()
+  --       return vim.fn["codeium#Clear"]()
+  --     end, { expr = true, silent = true })
+  --   end,
+  -- },
 }
