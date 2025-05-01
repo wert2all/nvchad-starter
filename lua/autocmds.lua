39cf393928c243fcb4174a459f9bf6030d39bc44
wert2all <wert2all@gmail.com>
2025-05-01 18:39:05 +0300

    fix: move autocmds to correct file

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,5 +1,16 @@
 require "nvchad.autocmds"
 
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- don't auto comment new line
+vim.api.nvim_create_autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
+
 vim.filetype.add {
   filename = {
     ["docker-compose.yml"] = "yaml.docker-compose",
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -57,14 +57,3 @@ map("n", "<C-u>", "<C-u>zz")
 
 map("n", "n", "nzzv")
 map("n", "N", "Nzzv")
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
--- don't auto comment new line
-vim.api.nvim_create_autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
