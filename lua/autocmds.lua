9cf29bdf3646cbe5bb4ff903add473e784e4cd95
Serhii Shvets <wert2all@gmail.com>
2025-05-01 18:35:48 +0300

    feat: extract autocmds config

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,10 @@
 require "nvchad.autocmds"
+
+vim.filetype.add {
+  filename = {
+    ["docker-compose.yml"] = "yaml.docker-compose",
+    ["docker-compose.yaml"] = "yaml.docker-compose",
+    ["compose.yml"] = "yaml.docker-compose",
+    ["compose.yaml"] = "yaml.docker-compose",
+  },
+}
