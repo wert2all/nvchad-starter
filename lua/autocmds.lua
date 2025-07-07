dde3d4fd8691f61db5f56616fec079c2361df41c
Serhii Shvets <wert2all@gmail.com>
2025-07-07 03:36:20 +0300

    refactor: move lsp autocommand to lspconfig

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -25,28 +25,3 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     vim.bo.filetype = "yaml.docker-compose"
   end,
 })
-
--- Lsp mappings
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local bufnr = args.buf
-    local map = vim.keymap.set
-
-    map("n", "gd", vim.lsp.buf.definition, { buffer = bufnr, desc = "Go to definition" })
-    map("n", "gD", vim.lsp.buf.declaration, { buffer = bufnr, desc = "Go to declaration" })
-
-    map({ "n", "x" }, "gra", vim.lsp.buf.code_action, { buffer = bufnr, desc = "Code action" })
-    map({ "n", "x" }, "<leader>la", vim.lsp.buf.code_action, { buffer = bufnr, desc = "Code action" })
-
-    map("n", "grn", vim.lsp.buf.rename, { buffer = bufnr, desc = "Rename" })
-    map("n", "<leader>lr", vim.lsp.buf.rename, { buffer = bufnr, desc = "Rename" })
-
-    map("n", "grr", vim.lsp.buf.references, { buffer = bufnr, desc = "References" })
-    map("n", "<leader>lR", vim.lsp.buf.references, { buffer = bufnr, desc = "References" })
-
-    map("n", "gri", vim.lsp.buf.implementation, { buffer = bufnr, desc = "Implementation" })
-    map("n", "gy", vim.lsp.buf.type_definition, { buffer = bufnr, desc = "Definition of current type" })
-
-    map({ "i", "s" }, "<C-S>", vim.lsp.buf.signature_help, { buffer = bufnr, desc = "Signature help" })
-  end,
-})
